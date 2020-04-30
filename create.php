d05d63a250a3fc745ce20a994af364153db2bda1
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-04-30 14:47:52 +0200

    creation formulaire creation de personnage

--- create.php
+++ create.php
@@ -1,11 +1,17 @@
 <?php include('partials/service.php')?>
 
 <?php 
+$thisUser = $_SESSION['user'];
+
+$request = "SELECT name FROM competence";
+$response = $bdd->query($request);
+$competences = $response->fetchAll(PDO::FETCH_COLUMN);
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head>  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game in Life</title>
@@ -15,15 +21,21 @@
     <h2>Reflet de vos compétences, passion et ambitions démesurées.</h2>
 
     <form action="create_traitment.php" method="POST">
-        <input type="hidden" value="user_id" name="id">
+        <input type="hidden" value=<?= $thisUser['id'] ?> name="id">
         <div>
             <label for="pseudo">Pseudo</label>
             <input type="text" name="pseudo" id="pseudo" placeholder="votre pseudo">
         </div>
-        <p>Ajoutez votre toute première compétence !</p>
+        
         <div>
-            <label for="competence">Pseudo</label>
-            <input type="text" name="competence" id="competence" placeholder="un de vos (nombreux) talents">
+            <label for="competence">Ajoutez votre toute première compétence !</label>
+
+            <select name="competence" id="competence">
+                <option value="no skill">Compléter plus tard</option>
+                <?php foreach($competences as $competence) :?>
+                    <option value=<?= $competence ?>><?= $competence ?></option>
+                <?php endforeach;?>
+            </select>
         </div>
         <input type="submit">
     </form>
--- create_traitment.php
+++ create_traitment.php
@@ -1,5 +1,7 @@
 <?php include('../partials/service.php')?>
 
 <?php 
-
+// $request = "SELECT * FROM user WHERE email = '$thisUserEmail'";
+// $response = $bdd->query($request);
+// $user = $response->fetch(PDO::FETCH_ASSOC);
 ?>
