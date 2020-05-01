fc3c73db2d5803d5ea4340ad920b68af1060fde3
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-05-01 13:50:40 +0200

    affichage dynamique page d'accueil

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
-<?php session_start()?>
+<?php include('partials/service.php')?>
+
+<?php
+    $isUserConnected = false;
+    $userHasCharacter = false;
+    
+    // Check if user is connected
+    if (isset($_SESSION['user'])) {
+        $isUserConnected = true;
+
+        $currentUser = $_SESSION['user'];
+        
+        $selectUserProfil = "SELECT * FROM user LEFT JOIN profil ON user.profil = profil.id";
+        $response = $bdd->query($selectUserProfil);
+        $userProfil = $response->fetch(PDO::FETCH_ASSOC);
+
+    } else {
+        $isUserConnected = false;
+    }
+
+    // Check if character has been created
+    if (!empty($userProfil['pseudo'])) {
+        $userHasCharacter = true;
+    } else {
+        $userHasCharacter = false;
+    }
+    
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,7 +38,7 @@
     
     <h1>Bienvenue sur Game in Life</h1>
 
-    <?php if (!isset( $_SESSION['user'])) : ?>
+    <?php if (!$isUserConnected) : ?>
         <p>Vous n'avez pas de compte ?</p>
         <button><a href="authentification/signup.php">Inscrivez-vous</a></button>
         
@@ -19,9 +46,15 @@
         <button><a href="authentification/login.php">Connectez-vous</a></button>
 
     <?php else : ?>
-        <h1>Bonjour <?= $_SESSION['user']['email'] ?></h1>
-        <p>Vous n'avez pas encore de personnage ?</p>
-        <button><a href="create.php">Créer votre personnage</a></button>
+        <h1>Bonjour <?= $userProfil['pseudo'] ?></h1>
+        
+        <?php if(!$userHasCharacter) : ?>
+            <p>Vous n'avez pas encore de personnage ?</p>
+            <button><a href="create.php">Créer votre personnage</a></button>
+        <?php else :?>
+            <p>Afficher votre personnage</p>
+            <button><a href="profil_page.php">Afficher votre personnage</a></button>
+        <?php endif; ?>
 
         <p>se déconnecter</p>
         <button><a href="authentification/logout.php">Déconnectez-vous</a></button>
---
+++ profil_page.php
@@ -0,0 +1,2 @@
+<?php include('partials/service.php')?>
+
