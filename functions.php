f1eb0e1a92e5e567c667fd0bdd79c633a0e1a857
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-05-04 16:06:03 +0200

    nettoyage fichier profil et index

--- functions.php
+++ functions.php
@@ -1,11 +1,19 @@
 <?php 
- function doRequest($bdd, $request) {
+function doRequest($bdd, $request) {
     $response = $bdd->query($request);
     return $response->fetchAll(PDO::FETCH_ASSOC);
- }
+}
 
- function doSingleRequest($bdd, $request) {
+function doSingleRequest($bdd, $request) {
     $response = $bdd->query($request);
     return $response->fetch(PDO::FETCH_ASSOC);
- }
+}
+
+function isArchived($element) {
+    if ($element == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
 ?>
--- profil_page.php
+++ profil_page.php
@@ -5,27 +5,23 @@
 <?php 
 
 if($isUserConnected == true) {
+    // Get user's profil
+    $requestUserProfil = "SELECT * FROM profil WHERE id = $profilId";
+    $userProfil = doSingleRequest($bdd, $requestUserProfil);
 
-// $currentProfilId = (int) $_SESSION['user']['profil'];
+    // Get user's competences
+    $requestUserCompetences = "SELECT * FROM competence_profil LEFT JOIN competence_list ON competence_profil.competence_id = competence_list.id WHERE profil_id = $profilId";
+    $competences = doRequest($bdd, $requestUserCompetences);
 
-// Get user's profil
-$requestUserProfil = "SELECT * FROM profil WHERE id = $profilId";
-$getUserProfil = $bdd->query($requestUserProfil);
-$userProfil = $getUserProfil->fetch(PDO::FETCH_ASSOC);
+    // Get user's profil image
+    $profilImage = $userProfil['profil_image'];
 
-// Get user's competences
-$requestUserCompetences = "SELECT * FROM competence_profil LEFT JOIN competence_list ON competence_profil.competence_id = competence_list.id WHERE profil_id = $profilId";
-$getCompetences = $bdd->query($requestUserCompetences);
-$competences = $getCompetences->fetchAll(PDO::FETCH_ASSOC);
+    //Get user's objectifs
+    $requestUserObjectifs = "SELECT * FROM objectif WHERE profil_id = $profilId";
+    $objectifs = doRequest($bdd, $requestUserObjectifs);
 
-//Get user's objectifs
-$requestUserObjectifs = "SELECT * FROM objectif WHERE profil_id = $profilId";
-$getObjectifs = $bdd->query($requestUserObjectifs);
-$objectifs = $getObjectifs->fetchAll(PDO::FETCH_ASSOC);
-
-// Get user's profil image
-$profilImage = $userProfil['profil_image'];
-
+} else {
+    echo "coming soon : Afficher une fiche personnage pour un utilisateur non connecté";
 }
 
 
@@ -44,20 +40,33 @@ $profilImage = $userProfil['profil_image'];
 
     <main class="profil-page">
         <div class="card">
+
+
+            <!-- Gestion des compétences -->
             <section class="competences">
                 <h2>Compétences</h2>
+
                 <?php foreach($competences as $competence) :?>
-                <ul>
-                    <li><strong><?= $competence['name'] ?> :</strong> <?= $competence['content'] ?></li>
-                    <li>State : <?= $competence['state_evol'] ?> %</li>
-                    <button class="profil-button"><a href="modifications/delete_traitment.php?competence=<?= $competence['competence_id'] ?>">Delete</a></button></li>
-                    <button class="profil-button"><a href="modifications/edit_competence.php?competence=<?= $competence['competence_id'] ?>">Edit</a></button></li>
-                </ul>
+                    <ul>
+                        <li>
+                            <strong><?= $competence['name'] ?> :</strong> <?= $competence['content'] ?>
+                        </li>
+
+                        <li>State : <?= $competence['state_evol'] ?> %</li>
+                        
+                        <li>
+                            <button class="profil-button"><a href="modifications/delete_traitment.php?competence=<?= $competence['competence_id'] ?>">Delete</a></button>
+                            <button class="profil-button"><a href="modifications/edit_competence.php?competence=<?= $competence['competence_id'] ?>">Edit</a></button>
+                        </li>
+                    </ul>
                 <?php endforeach;?>
             </section>
+            
 
+            <!-- Affichage du pseudo + image -->
             <section class="character">
                 <h1>Pseudo : <?= $userProfil['pseudo'] ?> </h1>
+
                 <?php if(empty($profilImage)) : ?>
                     <img src="assets/man-default-avatar.png" alt="" width="100%" height="auto">
                 <?php else : ?>
@@ -65,13 +74,23 @@ $profilImage = $userProfil['profil_image'];
                 <?php endif; ?>
             </section>
 
+            
+            <!-- gestion des objectifs -->
             <section class="objectifs">
                 <h2>Objectifs</h2>
+                
                 <?php foreach($objectifs as $objectif) :?>
-                <ul>
-                <li <?php if($objectif['archived'] == 1 ) :?> class="archived" <?php endif;?>><strong><?= $objectif['name'] ?> :</strong> <?= $objectif['content'] ?></li>
-                    <button class="profil-button disabled <?php if($objectif['archived'] == 1 ) :?> hidden <?php endif;?>"><a href="modifications/archive_traitment.php">Archive</a></button></li>
-                </ul>
+                    <ul>
+                        <li <?php if(isArchived($objectif['archived'])) :?> class="archived" <?php endif;?>>
+                            <strong><?= $objectif['name'] ?> :</strong> <?= $objectif['content'] ?>
+                        </li>
+
+                        <li>
+                            <button class="profil-button disabled <?php if(isArchived($objectif['archived'])) :?> hidden <?php endif;?>">
+                                <a href="modifications/archive_traitment.php">Archive</a>
+                            </button>
+                        </li>
+                    </ul>
                 <?php endforeach;?>
             </section>
             
